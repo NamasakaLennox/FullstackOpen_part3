732403b1416509fb41e7cf3d7da82bd86bc58d88
NamasakaLennox <namasakalennox254@gmail.com>
2023-05-15 20:46:05 +0300

    implemented a simple node server with express

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const app = express();
 
 app.use(express.json());
 
-const persons = [
+let persons = [
   {
     id: 1,
     name: "Arto Hellas",
@@ -26,17 +26,73 @@ const persons = [
   },
 ];
 
+// home page
 app.get("/", (request, response) => {
   response.send(
     "<div><h1>Hello World</h1><p>Welcome to my learning page</p></div>"
   );
 });
 
+const getNumPeople = () => persons.length;
+const generateId = () => Math.floor(Math.random() * 1000000);
+
+// get info summary page
 app.get("/info", (request, response) => {
-  response.send("Phonebook has info for ... people <br></br> blaaah"); //starting point tomorrow
+  const numPeople = getNumPeople();
+  response.send(
+    `<p>Phonebook has info for ${numPeople} people</p><p>${new Date()}</p>`
+  ); //starting point tomorrow
 });
 
+// get persons data
 app.get("/api/persons", (request, response) => {
+  response.json(persons);
+});
+
+// display single object
+app.get("/api/persons/:id", (request, response) => {
+  const id = Number(request.params.id);
+  const person = persons.find((person) => person.id == id);
+  if (person) {
+    response.json(person);
+  } else {
+    response.status(404).end();
+  }
+});
+
+// delete an object
+app.delete("/api/persons/:id", (request, response) => {
+  const id = Number(request.params.id);
+  persons = persons.filter((person) => person.id != id);
+  console.log(persons);
+  response.status(204).end();
+});
+
+// add a new object
+app.post("/api/persons", (request, response) => {
+  const body = request.body;
+  // handle if name or number is missing
+  if (!body.name || !body.number) {
+    return response.status(400).json({
+      error: "name or number is missing",
+    });
+  }
+
+  // handle if name exists
+  if (persons.find((person) => person.name == body.name)) {
+    return response.status(400).json({
+      error: "name must be unique",
+    });
+  }
+
+  const newPerson = {
+    id: generateId(),
+    name: body.name,
+    number: body.number,
+  };
+  console.log(newPerson);
+  persons = persons.concat(newPerson);
+
   response.json(persons);
 });
 
